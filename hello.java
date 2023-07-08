7969619e3165237cb5104e4091d3a4b4acb8055c
neeraj-singh <71137350+neeraj-pratap-singh@users.noreply.github.com>
2023-07-08 16:22:54 +0530

    updated hello world program

--- hello.java
+++ hello.java
@@ -1,7 +1,6 @@
 public class hello {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 10;
-        System.out.println(a>3 || b<3);
+        String x = "Hello World!";
+        System.out.println(x);
     }
 }
