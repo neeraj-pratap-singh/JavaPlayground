10bfaf5f298e68e9b4a1b6e7cfefabedc16e0c07
neeraj-singh <71137350+neeraj-pratap-singh@users.noreply.github.com>
2023-07-09 15:11:49 +0530

    write datatype sample code

---
+++ dataTypesINJava.java
@@ -0,0 +1,32 @@
+public class dataTypesINJava {
+    public static void main(String[] args) {
+        // boolean 
+        boolean myCheck = false;
+        System.out.println("The Value of myCheck Variable is: " + myCheck);
+
+        byte myAge = 15;
+        System.out.println("The age value: " + myAge);
+
+        short num = 1800;
+        System.out.println("The value of num: "+ num);
+
+        int myINTNum = 567410;
+        System.out.println("Integer num: " + myINTNum);
+
+        long longNum = 12345678;
+        System.out.println("Long Num: " + longNum);
+
+        float myFloatNum = 14.85f;
+        System.out.println("Float num: " + myFloatNum);
+
+        double myDoubleNum = 12345.9874;
+        System.out.println("Double num: " + myDoubleNum);
+
+        char answer = 'Y';
+        System.out.println("Answer is: " + answer);
+
+        String myCourseName = "FSD";
+        System.out.println("Course Name: " + myCourseName);
+        
+    }
+}
